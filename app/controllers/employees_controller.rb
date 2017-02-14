8f2d9a9f80b77815466d1db9ece288eb09702640
JackMarx <seriousfools@gmail.com>
2017-02-13 20:53:16 -0600

    poro complete

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -5,58 +5,41 @@ class EmployeesController < ApplicationController
   end
 
   def new
-    
+    @employee = Employee.new
   end
 
   def create
-    @employee = Unirest.post(
-                            "#{ ENV['API_HOST_URL'] }/api/v1/employees",
-                            headers: {
-                                      "Accept" => "application/json"
-                                      },
-                            parameters: {
-                                         first_name: params[:first_name],
-                                         last_name: params[:last_name],
-                                         email: params[:email]
-                                         }
-                            ).body
+    @employee = Employee.create(
+                                first_name: params[:first_name],
+                                last_name: params[:last_name],
+                                email: params[:email]
+                                )
 
-    redirect_to "/employees/#{@employee["id"]}"
+    redirect_to "/employees/#{@employee.id}"
   end
 
   def show
     @employee = Employee.find(params[:id])
-
-    @employee = Employee.new(Unirest.get("#{ ENV['API_HOST_URL'] }/api/v1/employees/#{params[:id]}.json").body)
   end
 
   def edit
-    @employee = Unirest.get("#{ ENV['API_HOST_URL'] }/api/v1/employees/#{params[:id]}.json").body
+    @employee = Employee.find(params[:id])
   end
 
   def update
-    @employee = Unirest.patch(
-                              "#{ ENV['API_HOST_URL'] }/api/v1/employees/#{params[:id]}",
-                              headers: {
-                                        "Accept" => "application/json"
-                                        },
-                              parameters: {
-                                           first_name: params[:first_name],
-                                           last_name: params[:last_name],
-                                           email: params[:email]
-                                           }
-                              ).body
+    @employee = Employee.find(params[:id])
+    @employee.update(
+                    first_name: params[:first_name],
+                    last_name: params[:last_name],
+                    email: params[:email]
+                    )
 
-    redirect_to "/employees/#{@employee["id"]}"
+    redirect_to "/employees/#{@employee.id}"
   end
 
   def destroy
-    @employee = Unirest.delete(
-                              "#{ ENV['API_HOST_URL'] }/api/v1/employees/#{params[:id]}",
-                              headers: {
-                                        "Accept" => "application/json"
-                                        }
-                              ).body
+    @employee = Employee.find(params[:id])
+    @employee.destroy
     redirect_to '/employees'
   end
 end
--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,12 +1,16 @@
 class Employee
   attr_accessor :id, :first_name, :last_name, :email, :birthday
 
-  def initialize(hash)
+  def initialize(hash={})
     @id = hash["id"]
     @first_name = hash["first_name"]
     @last_name = hash["last_name"]
     @email = hash["email"]
-    @birthday = Date.parse(hash["birthday"])
+    if hash["birthday"]
+      @birthday = Date.parse(hash["birthday"])
+    else
+      @birthday = nil
+    end
   end
 
   def full_name
@@ -14,7 +18,11 @@ class Employee
   end
 
   def friendly_birthday
-    birthday.strftime('%b %d, %Y')
+    if birthday
+      birthday.strftime('%b %d, %Y')
+    else
+      "No Birthday Listed"
+    end
   end
 
   def self.all
@@ -28,6 +36,37 @@ class Employee
 
   def self.find(employee_id)
     Employee.new(Unirest.get("#{ ENV['API_HOST_URL'] }/api/v1/employees/#{ employee_id }.json").body)
+  end
+
+  def destroy
+    Unirest.delete(
+                    "#{ ENV['API_HOST_URL'] }/api/v1/employees/#{ id }",
+                    headers: {
+                              "Accept" => "application/json"
+                              }
+                    ).body
+  end
+
+  def self.create(employee_info)
+    response_body = Unirest.post(
+                                "#{ ENV['API_HOST_URL'] }/api/v1/employees",
+                                headers: {
+                                          "Accept" => "application/json"
+                                          },
+                                parameters: employee_info
+                                ).body
+    Employee.new(response_body)
+  end
+
+  def update(employee_info)
+    response_body = Unirest.patch(
+                                  "#{ ENV['API_HOST_URL'] }/api/v1/employees/#{ id }",
+                                  headers: {
+                                            "Accept" => "application/json"
+                                            },
+                                  parameters: employee_info
+                                  ).body
+    Employee.new(response_body)
   end
 end
 
