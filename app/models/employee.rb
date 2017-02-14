cdf67994e3bc0b722320f5536037823253e3189f
JackMarx <seriousfools@gmail.com>
2017-02-13 21:17:12 -0600

    include headers with api requests

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,4 +1,10 @@
 class Employee
+  HEADERS = {
+              "Accept" => "application/json",
+              "X-User-Email" => ENV['API_EMAIL'],
+              "Authorization" => "Token token=#{ ENV['API_TOKEN'] }"
+              }
+
   attr_accessor :id, :first_name, :last_name, :email, :birthday
 
   def initialize(hash={})
@@ -27,7 +33,10 @@ class Employee
 
   def self.all
     employee_collection = []
-    api_employees = Unirest.get("#{ ENV['API_HOST_URL'] }/api/v1/employees.json").body
+    api_employees = Unirest.get(
+                                "#{ ENV['API_HOST_URL'] }/api/v1/employees.json",
+                                headers: HEADERS
+                                ).body
     api_employees.each do |employee_hash|
       employee_collection << Employee.new(employee_hash)
     end
@@ -35,24 +44,23 @@ class Employee
   end
 
   def self.find(employee_id)
-    Employee.new(Unirest.get("#{ ENV['API_HOST_URL'] }/api/v1/employees/#{ employee_id }.json").body)
+    Employee.new(Unirest.get(
+                             "#{ ENV['API_HOST_URL'] }/api/v1/employees/#{ employee_id }.json",
+                             headers: HEADERS
+                             ).body)
   end
 
   def destroy
     Unirest.delete(
                     "#{ ENV['API_HOST_URL'] }/api/v1/employees/#{ id }",
-                    headers: {
-                              "Accept" => "application/json"
-                              }
+                    headers: HEADERS
                     ).body
   end
 
   def self.create(employee_info)
     response_body = Unirest.post(
                                 "#{ ENV['API_HOST_URL'] }/api/v1/employees",
-                                headers: {
-                                          "Accept" => "application/json"
-                                          },
+                                headers: HEADERS,
                                 parameters: employee_info
                                 ).body
     Employee.new(response_body)
@@ -61,9 +69,7 @@ class Employee
   def update(employee_info)
     response_body = Unirest.patch(
                                   "#{ ENV['API_HOST_URL'] }/api/v1/employees/#{ id }",
-                                  headers: {
-                                            "Accept" => "application/json"
-                                            },
+                                  headers: HEADERS,
                                   parameters: employee_info
                                   ).body
     Employee.new(response_body)
